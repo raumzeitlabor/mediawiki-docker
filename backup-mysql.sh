0bf8100bb05f1789c9cb7b3403b143a7e00eeead
Simon Elsbrock <simon@iodev.org>
2015-03-15 22:09:17 +0100

    Compress mysql dumps and purge old dumps
    closes #4

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+DAYS_TO_KEEP=$(( 14 - 1))
 TARGET_DIR=/data/backup
 
 # *sigh*
@@ -12,8 +13,15 @@ MYSQL_PASSWD=$(php5 -r "$(grep wgDBadmin /data/LocalSettings.php; echo 'print $w
 
 echo "Backing up mediawiki database…"
 
-FILENAME=wiki-$(date '+%Y%m%d').sql
+FILENAME=wiki-$(date '+%Y%m%d').sql.xz
 TARGET_FILE=$TARGET_DIR/$FILENAME
-nice -n19 mysqldump -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASSWD $MYSQL_DB --opt -c > $TARGET_FILE
+nice -n19 mysqldump -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASSWD $MYSQL_DB --opt -c |
+nice -n19 xz -c > $TARGET_FILE
 md5sum $TARGET_FILE > $TARGET_FILE.md5sum
-chmod 600 $TARGET_DIR/*.sql
+chmod 600 $TARGET_DIR/*.sql.xz
+
+echo "Purging old backups…"
+
+find $TARGET_DIR -type f -mtime +13 -exec rm -v {} \;
+
+echo "done!"
